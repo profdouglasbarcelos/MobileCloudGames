3918ba0a45a8f1d31a97e5e012475ae12026d21a
profDouglasBarcelos <prof.douglasbarcelos@gmail.com>
2017-10-25 21:55:40 -0200

    Criando client para API de Itens

---
+++ MeuPrimeiroBackendN/CodigosAulasNoite/Assets/Scripts/ApiClient/ApiClient.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class ApiClient : MonoBehaviour
+{
+    const string baseUrl = "http://localhost:52258/API";
+
+	// Use this for initialization
+	void Start ()
+    {
+        StartCoroutine(GetItensApiAsync());
+	}
+	
+    IEnumerator GetItensApiAsync()
+    {
+        UnityWebRequest request = UnityWebRequest.Get(baseUrl + "/Itens");
+        yield return request.Send();
+
+        if(request.isNetworkError || request.isHttpError)
+        {
+            Debug.Log(request.error);
+        }
+        else
+        {
+            string response = request.downloadHandler.text;
+            //Debug.Log(response);
+
+            //byte[] bytes = request.downloadHandler.data;
+
+            Item[] itens = JsonHelper.getJsonArray<Item>(response);
+
+            foreach (Item i in itens)
+            {
+                ImprimirItem(i);
+            }
+
+        }
+    }
+
+    private void ImprimirItem(Item i)
+    {
+        Debug.Log("======= Dados Objeto ==========");
+
+        Debug.Log("Id: " + i.ItemID);
+        Debug.Log("Nome: " + i.Nome);
+        Debug.Log("Descrição: " + i.Descricao);
+        Debug.Log("Dano Máximo: " + i.DanoMaximo);
+        Debug.Log("Raridade: " + i.Raridade);
+        Debug.Log("TipoItemID: " + i.TipoItemID);
+    }
+}
---
+++ MeuPrimeiroBackendN/CodigosAulasNoite/Assets/Scripts/ApiClient/Models/Item.cs
@@ -0,0 +1,16 @@
+﻿[System.Serializable]
+public class Item
+{
+    public int ItemID;
+
+    public string Nome;
+
+    public string Descricao;
+
+    public int DanoMaximo;
+
+    public int Raridade;
+
+    // Relacionamento Item --> TipoItem
+    public int TipoItemID;
+}
