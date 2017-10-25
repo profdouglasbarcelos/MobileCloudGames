97786ec000a4448447e4d013aadafff0da2b122c
profDouglasBarcelos <prof.douglasbarcelos@gmail.com>
2017-10-25 18:24:47 -0200

    Adicionando primeira versao do client API na Unity; Atualizando o gitignore para Unity3D

---
+++ MeuPrimeiroBackendT/CodigosAulas/Assets/Scripts/ClientAPI/ApiClient.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class ApiClient : MonoBehaviour
+{
+    const string url = "http://localhost:50287/API/Itens/";
+
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(GetItensAPI());
+    }
+
+    IEnumerator GetItensAPI()
+    {
+        //UnityWebRequest request = UnityWebRequest.Get(url + "/1");
+        UnityWebRequest request = UnityWebRequest.Get(url);
+
+        yield return request.Send();
+
+        if (request.isNetworkError || request.isHttpError)
+        {
+            Debug.Log(request.error);
+        }
+        else
+        {
+            string strRespostaServidor = request.downloadHandler.text;
+            Debug.Log(strRespostaServidor);
+
+            byte[] result = request.downloadHandler.data;
+
+            //Item meuItem = JsonUtility.FromJson<Item>(strRespostaServidor);
+            //ImprimirItem(meuItem);
+
+            //*ListaItens listaItensServidor = new ListaItens();
+            //*JsonUtility.FromJsonOverwrite(strRespostaServidor, listaItensServidor);
+
+            Item[] teste = JsonHelper.getJsonArray<Item>(strRespostaServidor);
+
+            foreach (Item i in teste)
+            {
+                ImprimirItem(i);
+            }
+        }
+    }
+
+    void ImprimirItem(Item i)
+    {
+        Debug.Log("====== Dados objeto ======= ");
+        Debug.Log("ID: " + i.ItemID);
+        Debug.Log("Nome: " + i.Nome);
+        Debug.Log("Descrição: " + i.Descricao);
+
+    }
+}
