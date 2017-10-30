f2d597df1656a0e5397dd9e40cda460b6a435d88
profDouglasBarcelos <prof.douglasbarcelos@gmail.com>
2017-10-30 15:19:31 -0200

    Adicionando metodo para fazer POST na API atraves da Unity

--- MeuPrimeiroBackendT/CodigosUnityT/Assets/Scripts/ClientAPI/ApiClient.cs
+++ MeuPrimeiroBackendT/CodigosUnityT/Assets/Scripts/ClientAPI/ApiClient.cs
@@ -1,16 +1,43 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 
 public class ApiClient : MonoBehaviour
 {
-    const string url = "http://localhost:50287/API/Itens/";
+    const string url = "http://localhost:50287/API/Itens";
 
     // Use this for initialization
     void Start()
     {
         StartCoroutine(GetItensAPI());
+        StartCoroutine(PostItemAPI());
+    }
+
+    private IEnumerator PostItemAPI()
+    {
+        WWWForm form = new WWWForm();
+
+        form.AddField("nome", "ItemFromUnity");
+        form.AddField("descricao", "Item enviado pela unity (metodo POST)");
+        form.AddField("raridade", "50");
+        form.AddField("danoMaximo", "10");
+        form.AddField("tipoItemID", "1");
+
+        using (UnityWebRequest request = UnityWebRequest.Post(url + "/Create", form))
+        {
+            yield return request.SendWebRequest();
+
+            if(request.isNetworkError || request.isHttpError)
+            {
+                Debug.Log(request.error);
+            }
+            else
+            {
+                Debug.Log("Envio executado com sucesso");
+            }
+        }
     }
 
     IEnumerator GetItensAPI()
