c19f1814fbb5626c14e3bfd1142bdb1e4cb1db78
profDouglasBarcelos <prof.douglasbarcelos@gmail.com>
2017-11-08 17:25:23 -0200

    Uso da camera

---
+++ Tarde/ExemplosUnityT/Assets/Scripts/Webcam.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Webcam : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+        WebCamDevice[] devices = WebCamTexture.devices;
+        for (int i = 0; i < devices.Length; i++)
+            Debug.Log(devices[i].name);
+
+        WebCamTexture imagemWebCam = new WebCamTexture();
+
+        GetComponent<MeshRenderer>().material.mainTexture = imagemWebCam;
+
+        imagemWebCam.Play();
+    }
+}
