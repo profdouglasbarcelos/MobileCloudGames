887b5ffbfe0888e92f3c7b4311e6cc415802d6c0
profDouglasBarcelos <prof.douglasbarcelos@gmail.com>
2017-11-08 21:17:47 -0200

    Acelerometro e  Webcam

---
+++ Noite/ExemplosUnityN/Assets/Scripts/Acelerometro.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Acelerometro : MonoBehaviour
+{
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float x = Input.acceleration.x;
+        float z = -Input.acceleration.z;
+
+        transform.Translate(x, 0, z);
+	}
+}
---
+++ Noite/ExemplosUnityN/Assets/Scripts/WebCam.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WebCam : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+        WebCamTexture camTexture = new WebCamTexture();
+        GetComponent<MeshRenderer>().material.mainTexture = camTexture;
+        camTexture.Play();
+
+        // detectar todas cameras do dispositivo
+        WebCamDevice[] cameras = WebCamTexture.devices;
+
+        foreach (WebCamDevice cam in cameras)
+        {
+            Debug.Log("Camera: " + cam.name);
+        }
+    }
+	
+}
