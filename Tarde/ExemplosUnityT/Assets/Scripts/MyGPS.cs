17690b5536817b82ae24e141e56f59a861c4575a
profDouglasBarcelos <prof.douglasbarcelos@gmail.com>
2017-11-13 15:51:56 -0200

    Utilizando GPS

---
+++ Tarde/ExemplosUnityT/Assets/Scripts/MyGPS.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MyGPS : MonoBehaviour 
+{
+    // Singleton
+    public static MyGPS MyGPSInstance { get; set; }
+
+    public float latitude;
+    public float longitude;
+
+    public Text coordenadasHUD;
+
+	// Use this for initialization
+	void Start ()
+    {
+        MyGPSInstance = this;
+        DontDestroyOnLoad(gameObject);
+
+        StartCoroutine(StartGpsService());
+	}
+
+    private IEnumerator StartGpsService()
+    {
+        if(!Input.location.isEnabledByUser)
+        {
+            Debug.Log("Usuario nao permitiu compartilhamento da localizacao");
+            yield break;
+        }
+
+        // Inicializar o servico
+        Input.location.Start();
+
+        // Aguardando o dispositivo ficar pronto
+        int maxWait = 20;
+
+        while (Input.location.status == LocationServiceStatus.Initializing 
+            && maxWait > 0)
+        {
+            yield return new WaitForSeconds(1);
+            maxWait--;
+        }
+
+        // Testando se o servico nao inicializou
+        if(maxWait < 1)
+        {
+            Debug.Log("Timeout GPS");
+            yield break;
+        }
+
+
+        // houve falha ao inicializar o servico
+        if(Input.location.status == LocationServiceStatus.Failed)
+        {
+            Debug.Log("Nao foi possivel determinar sua localizacao");
+            yield break;
+        }
+        else
+        {
+            // Acesso garantio a localizacao do dispositivo
+            latitude = Input.location.lastData.latitude;
+            longitude = Input.location.lastData.longitude;
+
+            string coordenadas = "Lat: " + latitude + ", Lon: " + longitude;
+
+            Debug.Log(coordenadas);
+
+            coordenadasHUD.text = coordenadas;
+        }
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+		
+	}
+}
