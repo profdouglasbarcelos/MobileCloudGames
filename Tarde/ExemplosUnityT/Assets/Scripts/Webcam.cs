796d9fecf0d595bda13a34ba8444e48147e04b43
profDouglasBarcelos <prof.douglasbarcelos@gmail.com>
2017-11-20 16:49:46 -0200

    Adicionando objeto orbitando (AR básica)

--- Tarde/ExemplosUnityT/Assets/Scripts/Webcam.cs
+++ Tarde/ExemplosUnityT/Assets/Scripts/Webcam.cs
@@ -1,20 +1,54 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
-public class Webcam : MonoBehaviour {
+public class Webcam : MonoBehaviour
+{
+    WebCamTexture imagemWebCam;
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start ()
     {
         WebCamDevice[] devices = WebCamTexture.devices;
         for (int i = 0; i < devices.Length; i++)
             Debug.Log(devices[i].name);
 
-        WebCamTexture imagemWebCam = new WebCamTexture();
+        imagemWebCam = new WebCamTexture();
 
         GetComponent<MeshRenderer>().material.mainTexture = imagemWebCam;
 
         imagemWebCam.Play();
     }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            StartCoroutine(CaptureTextureAsPNG());
+        }
+    }
+
+
+    IEnumerator CaptureTextureAsPNG()
+    {
+        Debug.Log("Apertou");
+
+        yield return new WaitForEndOfFrame();
+        Texture2D _TextureFromCamera = 
+            new Texture2D(
+                GetComponent<Renderer>().material.mainTexture.width,
+                GetComponent<Renderer>().material.mainTexture.height
+                );
+        _TextureFromCamera.SetPixels(
+            (GetComponent<Renderer>().material.mainTexture as WebCamTexture).GetPixels());
+        _TextureFromCamera.Apply();
+        byte[] bytes = _TextureFromCamera.EncodeToPNG();
+        string filePath = "SavedScreen1.png";
+
+        File.WriteAllBytes(filePath, bytes);
+
+    }
+
+
+
 }
