9642f6bd52693ce1c7a6571c1b3fa94582e932d5
profDouglasBarcelos <prof.douglasbarcelos@gmail.com>
2017-11-29 16:47:50 -0200

    Girando um objeto de acordo com o acelerometro

---
+++ Tarde/ExemplosUnityT/Assets/Scripts/RotateAccelerometer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateAccelerometer : MonoBehaviour
+{
+    private Quaternion localRotation; // controle local da rotacao
+    public float velocidade = 1.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+        // ajustando a rotacao inicial do objeto com o controle local
+        localRotation = transform.rotation;
+    }
+
+
+    void Update()
+    {
+        // velocidade considerando o deltaTime
+        float velocidadeAtual = Time.deltaTime * velocidade;
+
+        // primeiro atualize o controle interno de acordo com o input do acelerometro
+        localRotation.y += Input.acceleration.x * velocidadeAtual;
+        //localRotation.x += Input.acceleration.y * velocidadeAtual;
+
+        // rotacionar o objeto conforme o novo angulo
+        transform.rotation = localRotation;
+    }
+}
