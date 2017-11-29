09018c76e93384c0a642b1fb1d1a592f8c596e46
profDouglasBarcelos <prof.douglasbarcelos@gmail.com>
2017-11-29 18:15:53 -0200

    Alterando a cena de exemplo e script do GPS (start, stop)

--- Tarde/ExemplosUnityT/Assets/Scripts/MyGPS.cs
+++ Tarde/ExemplosUnityT/Assets/Scripts/MyGPS.cs
@@ -13,17 +13,18 @@ public class MyGPS : MonoBehaviour
 
     public Text latitudeHUD;
     public Text longitudeHUD;
+    public Text statusHUD;
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start ()
     {
         MyGPSInstance = this;
         DontDestroyOnLoad(gameObject);
 
-        StartCoroutine(StartGpsService());
+        //StartCoroutine(StartGpsService());
 	}
 
-    private IEnumerator StartGpsService()
+    public IEnumerator InitializeGpsService()
     {
         if(!Input.location.isEnabledByUser)
         {
@@ -60,7 +61,7 @@ public class MyGPS : MonoBehaviour
         }
         else
         {
-            // Acesso garantio a localizacao do dispositivo
+            // Acesso garantido a localizacao do dispositivo
             latitude = Input.location.lastData.latitude;
             longitude = Input.location.lastData.longitude;
 
@@ -70,17 +71,39 @@ public class MyGPS : MonoBehaviour
             SendInformation(lat, lon);
         }
     }
+    public void StartGpsService()
+    {
+        Debug.Log("Inicializando GPS");
+        StartCoroutine(InitializeGpsService());
+    }
+
+    public void StopGpsService()
+    {
+        Debug.Log("Encerrando GPS");
+        Input.location.Stop();
+        SendInformation("Lat: --", "Lon: --");
+    }
 
     private void SendInformation(string lat, string lon)
     {
         Debug.Log(lat + lon);
         latitudeHUD.text = lat;
         longitudeHUD.text = lon;
+        statusHUD.text = Input.location.status.ToString();
     }
 
     // Update is called once per frame
     void Update ()
     {
-		
-	}
+        if (Input.location.status == LocationServiceStatus.Running)
+        {
+            latitude = Input.location.lastData.latitude;
+            longitude = Input.location.lastData.longitude;
+
+            string lat = "Lat: " + latitude;
+            string lon = "Lon: " + longitude;
+
+            SendInformation(lat, lon);
+        }
+    }
 }
