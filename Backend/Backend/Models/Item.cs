f50fb7b1085f247994f0250b143b77a1aee34b63
profDouglasBarcelos <prof.douglasbarcelos@gmail.com>
2018-10-24 15:06:15 -0300

    Criando modelos

---
+++ Backend/Backend/Models/Item.cs
@@ -0,0 +1,18 @@
+﻿namespace Backend.Models
+{
+    public class Item
+    {
+        public int ItemID { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Descricao { get; set; }
+
+        // relacionamento TipoItem <--> Item
+
+        public int TipoItem { get; set; }
+
+        public virtual TipoItem _TipoItem { get; set; }
+
+    }
+}
---
+++ Backend/Backend/Models/TipoItem.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Backend.Models
+{
+    public class TipoItem
+    {
+        [Key]
+        public int TipoItemID { get; set; }
+
+        public string Nome { get; set; }
+    }
+}
