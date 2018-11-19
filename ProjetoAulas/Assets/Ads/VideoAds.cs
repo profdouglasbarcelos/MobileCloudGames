1ae8c5e532a60d16c644ae685ecd7e0db36f77ec
profDouglasBarcelos <prof.douglasbarcelos@gmail.com>
2018-11-19 20:21:58 -0200

    Criando script para video ads

---
+++ ProjetoAulas/Assets/Ads/VideoAds.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Monetization;
+
+public class VideoAds : MonoBehaviour {
+
+//#if UNITY_ANDROID
+    string gameID = "2920087";
+    bool testMode = true;
+//#endif
+
+    // Use this for initialization
+    void Start () {
+        // Inicializar monetizacao
+        Monetization.Initialize(gameID, testMode);
+	}
+	
+    public void btnVideo_Click()
+    {
+        StartCoroutine(MostrarVideoAds());
+    }
+
+    private IEnumerator MostrarVideoAds()
+    {
+        while(!Monetization.IsReady("video"))
+        {
+            yield return new WaitForSeconds(0.25f);
+        }
+
+        ShowAdPlacementContent video = Monetization.GetPlacementContent("video") as ShowAdPlacementContent;
+
+        if(video != null)
+        {
+            video.Show();
+        }
+    }
+}
