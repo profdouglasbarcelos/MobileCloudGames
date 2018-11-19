a104b6104851051c2b41213272f240682bb203dc
profDouglasBarcelos <prof.douglasbarcelos@gmail.com>
2018-11-19 17:01:22 -0200

    Adicionando Unity Ads SDK (Unity Monetization 3.0) e criando script para apresentar ads videos

---
+++ AulasMobileCloud/Assets/Monetization/SimpleVideoAds.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Monetization;
+
+public class SimpleVideoAds : MonoBehaviour {
+
+    string gameId = "2919836";
+    bool testMode = true;
+
+	// Use this for initialization
+	void Start () {
+        Monetization.Initialize(gameId, testMode);
+	}
+	
+	public void ShowAds()
+    {
+        StartCoroutine(ShowAdsVideo());
+    }
+
+    private IEnumerator ShowAdsVideo()
+    {
+        while(!Monetization.IsReady("video"))
+        {
+            yield return new WaitForSeconds(0.25f);
+        }
+
+        ShowAdPlacementContent content = null;
+
+        content = Monetization.GetPlacementContent("video") as ShowAdPlacementContent;
+
+        if(content != null)
+        {
+            content.Show();
+        }
+    }
+}
