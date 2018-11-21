c085b50b8fac37dd27b69f1cafbda5dff2f0b96e
profDouglasBarcelos <prof.douglasbarcelos@gmail.com>
2018-11-21 20:52:47 -0200

    Integrando Ads (banner)

---
+++ ProjetoAulas/Assets/Ads/BannerAds.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class BannerAds : MonoBehaviour {
+
+    string gameID = "2917751";
+    bool testMode = true;
+
+	// Use this for initialization
+	void Start () {
+        Advertisement.Initialize(gameID, testMode);
+        StartCoroutine(MostrarBanner());
+	}
+	
+	IEnumerator MostrarBanner()
+    {
+        while (!Advertisement.IsReady("banner"))
+        {
+            yield return new WaitForSeconds(0.5f);
+        }
+
+        Debug.Log("Exibir o banner");
+
+        Advertisement.Banner.Show("banner");
+    }
+}
