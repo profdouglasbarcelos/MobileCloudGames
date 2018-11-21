e01657ec685273e17569f01572fa8c30ae488f2a
profDouglasBarcelos <prof.douglasbarcelos@gmail.com>
2018-11-21 15:08:54 -0200

    Apresentando banner com UnityAds

---
+++ AulasMobileCloud/Assets/Monetization/BannerAds.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class BannerAds : MonoBehaviour {
+
+    private string gameID = "";
+    private bool testMode = true;
+
+    private void Start()
+    {
+        StartCoroutine(ShowBanner());
+    }
+
+    IEnumerator ShowBanner()
+    {
+        while(!Advertisement.IsReady("banner"))
+        {
+            yield return new WaitForSeconds(0.5f);
+        }
+
+        Advertisement.Banner.Show("banner");
+    }
+}
