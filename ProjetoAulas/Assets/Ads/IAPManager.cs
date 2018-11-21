4701035160f65d400ce21fa99751846ad382da39
profDouglasBarcelos <prof.douglasbarcelos@gmail.com>
2018-11-21 21:13:11 -0200

    Implementando IAP (via codeless)

---
+++ ProjetoAulas/Assets/Ads/IAPManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IAPManager : MonoBehaviour {
+    
+    public void ComprouItem(string item)
+    {
+        Debug.Log("Item comprado: " + item);
+    }
+
+    public void FalhaAoComprarItem(string item)
+    {
+        Debug.Log("Falha ao tentar comprar item: " + item);
+    }
+}
